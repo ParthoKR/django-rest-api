5d4c94e54fdc4c0ffaf5f85e0cace6c366aca355
ParthoKR <partho.rajvor2016@gmail.com>
2020-05-05 22:46:16 +0600

    Updated with include

--- profile_api/profile_api/urls.py
+++ profile_api/profile_api/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 urlpatterns = [
     path('app/', include('basic_rest_api.urls')),
     path('admin/', admin.site.urls),
