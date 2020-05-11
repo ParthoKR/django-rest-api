2b832b401a0d595f05392776dcd7761643221f9d
ParthoKR <partho.rajvor2016@gmail.com>
2020-05-11 18:01:29 +0600

    Added app to admin

--- profile_api/basic_rest_api/models.py
+++ profile_api/basic_rest_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.email
+
+
