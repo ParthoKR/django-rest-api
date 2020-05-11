d872403509231ed3207588bb26565d9e816aec9b
ParthoKR <partho.rajvor2016@gmail.com>
2020-05-12 02:09:02 +0600

    Added authentication to viewset

--- profile_api/basic_rest_api/models.py
+++ profile_api/basic_rest_api/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 
 class UserProfile(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     email = models.EmailField()
-    website = models.URLField(blank=True)
-    address = models.CharField(max_length=200)
-
+    password = models.CharField(max_length=200)
     def __str__(self):
         return self.email
--- profile_api/basic_rest_api/views.py
+++ profile_api/basic_rest_api/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from . import serializers
+from . import models
+from rest_framework.authentication import TokenAuthentication
+from . import permissions
 class HelloApiView(APIView):
     """Test API View"""
     serializer_class = serializers.HelloSerializer
@@ -88,4 +91,11 @@ class HelloViewSet(viewsets.ViewSet):
         return Response({'http_method' : 'DELETE'})
     
     
-    
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle create and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+
+
